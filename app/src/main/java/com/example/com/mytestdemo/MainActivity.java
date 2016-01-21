b9890219e8669862029115e8a691e9a92cac0b4a
pengpeng <pengpeng@mmclick.com>
2016-01-21 11:49:11 +0800

    增加gif jar包

--- app/src/main/java/com/example/com/mytestdemo/MainActivity.java
+++ app/src/main/java/com/example/com/mytestdemo/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.com.mytestdemo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Date date = new Date(1458040549348l);
+        Log.i("aaa_", String.valueOf(new Date().getTime()));
+        Log.i("aaa_", (date.getMonth()+1) + "," + date.getDate() + "," + date.getTime());
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        Log.i("aaa_", (cal.get(Calendar.MONTH)+1) + "," + cal.get(Calendar.DAY_OF_MONTH));
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        toolbar.setTitle("jjj");
 //        setSupportActionBar(toolbar);
--- app/src/main/java/com/example/com/mytestdemo/SwipeBackActivity.java
+++ app/src/main/java/com/example/com/mytestdemo/SwipeBackActivity.java
@@ -6,7 +6,10 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.ant.liao.GifView;
 import com.jude.swipbackhelper.SwipeBackHelper;
 import com.jude.swipbackhelper.SwipeListener;
 
@@ -23,6 +26,15 @@ public class SwipeBackActivity extends AppCompatActivity {
         setTitle("activity_" + i);
         i++;
         setContentView(R.layout.activity_swipeback);
+        initGif();
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_label);
+        //使用数组作为数据源
+        final String arr[] = new String[]{"周一", "周二", "周三", "周四", "周五", "周六",
+                "周七"};
+        // adpater对象
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arr);
+        spinner.setAdapter(arrayAdapter);
 
         findViewById(R.id.my_view).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +66,21 @@ public class SwipeBackActivity extends AppCompatActivity {
                     public void onScrollToClose() {//当滑动关闭
                     }
                 });
+    }
+
+    private void initGif() {
+
+        GifView gf1;
+        // 从xml中得到GifView的句柄
+        gf1 = (GifView) findViewById(R.id.imageView);
+        // 设置Gif图片源
+        gf1.setGifImage(R.drawable.fengshui);
+        // 添加监听器
+//        gf1.setOnClickListener(this);
+        // 设置显示的大小，拉伸或者压缩
+        gf1.setShowDimension(600, 250);
+        // 设置加载方式：先加载后显示、边加载边显示、只显示第一帧再显示
+        gf1.setGifImageType(GifView.GifImageType.COVER);
     }
 
 
